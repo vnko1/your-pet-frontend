6fa4ea2e6e33c7311039c0c6d5d9f89069bf221b
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-07-21 15:53:17 +0300

    add hover and focus on interactive elements of card for example

--- src/modules/notices/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/modules/notices/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,66 +1,55 @@
 import {
   AddToFavorite,
   Card,
+  CardTextInfoWrap,
   Category,
   City,
   Gender,
+  HeartIconWrap,
   Image,
   ImageWrap,
+  LearnMoreBtn,
+  Title,
   Years,
 } from "./NoticeCategoryItem.styled";
-
 
 function NoticesCategoryItem() {
   return (
     <Card>
       <ImageWrap>
-        <Image   
+        <Image
           src="
 https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"
           alt="cat"
         />
-        <AddToFavorite>Add f</AddToFavorite>
+        <AddToFavorite>
+          <HeartIconWrap>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M4.3314 12.0474L12 20L19.6686 12.0474C20.5211 11.1633 21 9.96429 21 8.71405C21 6.11055 18.9648 4 16.4543 4C15.2487 4 14.0925 4.49666 13.24 5.38071L12 6.66667L10.76 5.38071C9.90749 4.49666 8.75128 4 7.54569 4C5.03517 4 3 6.11055 3 8.71405C3 9.96429 3.47892 11.1633 4.3314 12.0474Z"
+                stroke="#54ADFF"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </HeartIconWrap>
+        </AddToFavorite>
         <Category>In good hands</Category>
         <City>Lviv</City>
         <Years>1 year</Years>
         <Gender>female</Gender>
       </ImageWrap>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 20,
-          padding: "20px 24px",
-        }}
-      >
-        <h2
-          style={{
-            margin: 0,
-            padding: 0,
-
-            fontFamily: "Manrope",
-            fontSize: 24,
-            fontStyle: "normal",
-            fontWeight: 700,
-            lineHeight: "normal",
-          }}
-        >
-          Сute dog looking for a home
-        </h2>
-        <button
-          style={{
-            display: "flex",
-            width: 248,
-            padding: "8px 28px",
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 40,
-            border: "2px solid #54ADFF",
-          }}
-        >
-          Learn more
-        </button>
-      </div>
+      <CardTextInfoWrap>
+        <Title>Сute dog looking for a home</Title>
+        <LearnMoreBtn>Learn more</LearnMoreBtn>
+      </CardTextInfoWrap>
     </Card>
   );
 }
--- src/modules/notices/components/NoticeCategoryItem/NoticeCategoryItem.styled.js
+++ src/modules/notices/components/NoticeCategoryItem/NoticeCategoryItem.styled.js
@@ -6,7 +6,9 @@ export const Card = styled.li`
   padding: 0;
   width: 288px;
   height: 456px;
-  background-color: gray;
+  background-color: #fff;
+  border-radius: 0px 0px 40px 40px;
+  box-shadow: 3px 8px 14px 0px rgba(136, 198, 253, 0.19);
 `;
 
 export const ImageWrap = styled.div`
@@ -27,7 +29,22 @@ export const AddToFavorite = styled.button`
   right: 12px;
   width: 40px;
   height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-radius: 50%;
+  border: none;
+`;
+
+export const HeartIconWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover svg path,
+  &:focus svg path {
+    fill: #54adff;
+  }
 `;
 
 export const Category = styled.p`
@@ -97,4 +114,49 @@ export const Gender = styled.p`
   gap: 4px;
   border-radius: 16px;
   background: #cce4fb;
+`;
+
+export const CardTextInfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px 24px;
+`;
+
+export const Title = styled.h2`
+  margin: 0;
+  padding: 0;
+
+  font-family: Manrope;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color: #111;
+`;
+
+export const LearnMoreBtn = styled.button`
+  display: flex;
+  width: 248px;
+  padding: 8px 28px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  font-family: Manrope;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: 0.64px;
+
+  border-radius: 40px;
+  border: 2px solid #54adff;
+  color: #54adff;
+
+  &:hover,
+  &:focus {
+    color: #fef9f9;
+    background: linear-gradient(315deg, #419ef1 0%, #9bd0ff 100%);
+  }
 `;
