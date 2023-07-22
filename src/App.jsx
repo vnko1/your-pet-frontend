a4c45cca28c85b31ab1cc570b0d1384428cbb47a
Evgeniy <gerardcobain1@gmail.com>
2023-07-22 17:16:53 +0300

    Create MainPage
    Створений компонент,але поки ще нема куди його підключити,але створено та підігнані стилі,після роутів можна буде вже підключити та виправити помилки якшшо будуть

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
-
-  return (
-    <div>
-      test
-      </div>
-  )
+  return <div>test</div>;
 }
 
-export default App
+export default App;
