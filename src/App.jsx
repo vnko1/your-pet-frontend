00148109b38b2ae5b5d0dfeef936ff32fb739382
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2023-07-22 16:19:27 +0300

    redux-auth
    -add base url(for import files)(jsconfig.json)
    -add redux-auth
    -add persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div>test</div>;
-}
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { refreshUser } from "./redux/auth/auth-operations";
+import authSelectors from "./redux/auth/auth-selectors";
 
-export default App;
+export default function App() {
+  const dispath = useDispatch();
+  const isRefreshing = useSelector(authSelectors.selectRefreshing);
+
+  useEffect(() => {
+    dispath(refreshUser());
+  }, [dispath]);
+
+  return isRefreshing ? (
+    <div>Вставить Спінер або щось що інформує про загрузку !</div>
+  ) : (
+    <div>Тут Блок Навігації</div>
+  );
+}
