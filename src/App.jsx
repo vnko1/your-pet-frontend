23e157dc59dd5ecc390ad9f783ffec97540b7c7c
Грона Оля <grona.hell@gmail.com>
2023-07-24 14:11:09 +0300

    delete coments in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-// import sprite from "./assets/icons.svg";
-// import {
-//   LearnMoreWideWhiteBtn,
-//   LearnMoreWideBlueBtn,
-//   LearnMoreSmallBlueBtn,
-//   LearnMoreSmallWhiteBtn,
-//   PawIcon,
-//   PawBlueIcon,
-//   FilterBtn,
-//   FilterIcon,
-//   LocationBtn,
-//   LocationIcon,
-//   TagsBtn,
-//   FavouriteBtn,
-//   HeartIcon,
-//   Input,
-//   InputSmall,
-//   SearchIcon,
-//   SearchBtn,
-//   SearchContainer,
-// } from "./App.styled";
 import SharedComponents from "./shared/components/SharedComponents/SharedComponents";
 import { Container } from "./styles/Container";
 
@@ -27,60 +6,6 @@ function App() {
     <Container>
       test
       <SharedComponents />
-      {/* <LearnMoreWideWhiteBtn>Learn more</LearnMoreWideWhiteBtn>
-      <LearnMoreWideBlueBtn>
-        Learn more
-        <PawIcon>
-          <use href={sprite + "#pawprint"} />
-        </PawIcon>
-      </LearnMoreWideBlueBtn>
-      <LearnMoreSmallBlueBtn>
-        Learn more{" "}
-        <PawIcon>
-          <use href={sprite + "#pawprint"} />
-        </PawIcon>
-      </LearnMoreSmallBlueBtn>
-      <LearnMoreSmallWhiteBtn>
-        Learn more{" "}
-        <PawBlueIcon>
-          <use href={sprite + "#pawprint"} />
-        </PawBlueIcon>
-      </LearnMoreSmallWhiteBtn>
-      {/* <button>Add pet</button> */}
-      {/* <FilterBtn>
-        Filter
-        <FilterIcon>
-          <use href={sprite + "#filters"} />
-        </FilterIcon>
-      </FilterBtn>
-      <LocationBtn>
-        <LocationIcon>
-          <use href={sprite + "#location"} />
-        </LocationIcon>
-        Lviv
-      </LocationBtn>
-      <TagsBtn>lost/found</TagsBtn>
-      <FavouriteBtn>
-        <HeartIcon>
-          <use href={sprite + "#heart"} />
-        </HeartIcon>
-      </FavouriteBtn>
-      <SearchContainer>
-        <Input type="text" autoComplete="off" placeholder="Search" />
-        <SearchBtn>
-          <SearchIcon>
-            <use href={sprite + "#search"} />
-          </SearchIcon>
-        </SearchBtn>
-      </SearchContainer>
-      <SearchContainer>
-        <InputSmall type="text" autoComplete="off" placeholder="Search" />
-        <SearchBtn>
-          <SearchIcon>
-            <use href={sprite + "#search"} />
-          </SearchIcon>
-        </SearchBtn>
-      </SearchContainer> */}
     </Container>
   );
 }
