4cc088fe9371002d9e24c4bb14378a63f72301f5
Danil Krasnov <99325740+Hellblazer98@users.noreply.github.com>
2023-07-25 11:14:20 +0300

    fix: vercel deploy

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/your-pet-frontend">
+        <BrowserRouter>
           <ThemeProvider theme={theme}>
             <GlobalStyles />
             <FontStyles />
