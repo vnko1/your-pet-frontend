6035082ee085173c363162c6a7ed2d69946039dc
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-07-25 11:46:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
-// import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <BrowserRouter basename="/your-pet-frontend"> */}
         <App />
-        {/* </BrowserRouter> */}
       </PersistGate>
     </Provider>
   </React.StrictMode>
