225f8dbf7122def1193ef8ac25be56dfba286b45
Danil Krasnov <99325740+Hellblazer98@users.noreply.github.com>
2023-07-25 13:08:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    base: "/your-pet-frontend/",
     open: "/index.html",
     port: 8000,
   },
