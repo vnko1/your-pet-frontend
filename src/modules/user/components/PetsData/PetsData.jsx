343ebff97f03cd7ab77227f34080a494530de8ae
Irina40Sigova <mishinaira1@gmail.com>
2023-07-28 12:26:37 +0300

    Update PetsData.jsx

--- src/modules/user/components/PetsData/PetsData.jsx
+++ src/modules/user/components/PetsData/PetsData.jsx
@@ -1,4 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useLocation } from "react-router-dom";
+
+import { selectPets } from "./../../../../redux/pets/pets-selectors";
+import { postPet } from "./../../../../redux/pets/pets-operation";
+import { deletePet } from "./../../../../redux/pets/pets-operation";
+
 import {
   UserPetsBody,
   UserPetsBtn,
@@ -13,43 +19,75 @@ import {
   UserPetsTitleWrap,
   UserPetsBtnDell,
 } from "./PetsData.styled";
+import { useEffect } from "react";
+
 const PetsData = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const pets = useSelector(selectPets);
+
+  useEffect(() => {
+    dispatch(postPet());
+  }, [dispatch]);
+
+  const handelDellPets = (id) => {
+    dispatch(deletePet(id));
+
+    dispatch(postPet());
+  };
+
   return (
     <UserPetsBody>
       <div>
         <UserPetsTitleWrap>
           <UserPetsTitle>My pets:</UserPetsTitle>
-          <NavLink>
+          <NavLink to={`add-pet`} state={location.pathname}>
             <UserPetsNavBtn>Add Pet +</UserPetsNavBtn>
           </NavLink>
         </UserPetsTitleWrap>
-        <NoUserPets>
-          You havenot added a pet yet, you have the option to add your furry
-          friend!
-        </NoUserPets>
-        <UserPetsInfo>
-          <UserPetsImg src alt="pet_image" />
-          <UserPetsBtnDell>
-            <svg />
-          </UserPetsBtnDell>
-          <UserPetsList>
-            <UserPetsItem>
-              Name: <UserPetsItemSpan> {}</UserPetsItemSpan>
-            </UserPetsItem>
-            <UserPetsItem>
-              Date of birth: <UserPetsItemSpan> {}</UserPetsItemSpan>
-            </UserPetsItem>
-            <UserPetsItem>
-              Breed: <UserPetsItemSpan> {}</UserPetsItemSpan>
-            </UserPetsItem>
-            <UserPetsItem>
-              Comments: <UserPetsItemSpan>{}</UserPetsItemSpan>
-            </UserPetsItem>
-          </UserPetsList>
-          <UserPetsBtn type="button">
-            <svg />
-          </UserPetsBtn>
-        </UserPetsInfo>
+        {pets.length === 0 ? (
+          <NoUserPets>
+            You havenot added a pet yet, you have the option to add your furry
+            friend!
+          </NoUserPets>
+        ) : (
+          <>
+            {pets.map(({ _id, petsURL, name, birthday, type, comments }) => {
+              <UserPetsInfo key={_id}>
+                <UserPetsImg src={petsURL} alt="pet_image" />
+                <UserPetsBtnDell>
+                  <svg />
+                </UserPetsBtnDell>
+                <UserPetsList>
+                  <UserPetsItem>
+                    Name:{""}
+                    <UserPetsItemSpan> {name}</UserPetsItemSpan>
+                  </UserPetsItem>
+                  <UserPetsItem>
+                    Date of birth:{" "}
+                    <UserPetsItemSpan> {birthday}</UserPetsItemSpan>
+                  </UserPetsItem>
+                  <UserPetsItem>
+                    Type:{""}
+                    <UserPetsItemSpan> {type}</UserPetsItemSpan>
+                  </UserPetsItem>
+                  <UserPetsItem>
+                    Comments:{""}
+                    <UserPetsItemSpan>{comments}</UserPetsItemSpan>
+                  </UserPetsItem>
+                </UserPetsList>
+                <UserPetsBtn
+                  type="button"
+                  onClick={() => {
+                    handelDellPets(_id);
+                  }}
+                >
+                  <svg />
+                </UserPetsBtn>
+              </UserPetsInfo>;
+            })}
+          </>
+        )}
       </div>
     </UserPetsBody>
   );
