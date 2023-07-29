cb19e79076556a5757f896fba04a3e973c27fb4a
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-07-29 11:25:29 +0300

    props

--- src/protected routers/PrivateRoute.js
+++ src/protected routers/PrivateRoute.js
@@ -1,9 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import PropTypes from "prop-types";
 
-// eslint-disable-next-line react/prop-types
 export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+};
+PrivateRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+  redirectTo: PropTypes.string,
 };
