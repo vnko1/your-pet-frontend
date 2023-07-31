d0a740ae824d6591c20987127f8e6cb432575d35
Irina40Sigova <mishinaira1@gmail.com>
2023-07-31 13:37:07 +0300

    вставила снінер

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,22 +1,15 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
 import { UserPageBody } from "./UserPage.styled";
-
-import authSelectors from "../../redux/auth/auth-selectors";
 
 import UserData from "../../modules/user/components/UserData/UserData";
 import PetsData from "../../modules/user/components/PetsData/PetsData";
 import { ModalCongrats } from "../../modules/user/components/ModalCongrats/ ModalCongrats";
 
 const UserPage = () => {
-  const [modal, setModal] = useState(false);
-  const time = useSelector(authSelectors.selectUser);
-
   return (
     <UserPageBody>
       <UserData />
       <PetsData />
-      {modal && <ModalCongrats />}
+      <ModalCongrats />
     </UserPageBody>
   );
 };
