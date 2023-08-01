03fc1052ca01ff45be9f1527174a4a83dfa4bc2d
Irina40Sigova <mishinaira1@gmail.com>
2023-08-01 08:00:20 +0300

    Update UserPage.jsx

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,15 +1,20 @@
+import { useSelector } from "react-redux";
 import { UserPageBody } from "./UserPage.styled";
 
 import UserData from "../../modules/user/components/UserData/UserData";
 import PetsData from "../../modules/user/components/PetsData/PetsData";
 import { ModalCongrats } from "../../modules/user/components/ModalCongrats/ ModalCongrats";
 
+import authSelectors from "../../redux/auth/auth-selectors";
+
 const UserPage = () => {
+  const isNewUser = useSelector(authSelectors.selectUser);
+
   return (
     <UserPageBody>
+      {!isNewUser && <ModalCongrats />}
       <UserData />
       <PetsData />
-      <ModalCongrats />
     </UserPageBody>
   );
 };
