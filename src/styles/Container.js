497c362b55514720154d56929fa9a5c5cea171b3
Butcherok <Butcherok231@gmail.com>
2023-08-02 08:36:06 +0300

    fix: container

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -4,9 +4,6 @@ import bgTablet from "../assets/bgTablet.svg";
 import bgMobile from "../assets/bgMobile.svg";
 
 export const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
 	width: 100%;
 	min-height: 100vh;
 	margin: 0 auto;
